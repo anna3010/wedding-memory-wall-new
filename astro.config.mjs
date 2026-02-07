753ab43edb8aa225de6a22dcb99ac1242e535cc2
ishaza arianna <ariadnelovecraft@gmail.com>
2026-02-07 12:53:00 +0800

    Add back environment variables for development mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import react from '@astrojs/react';
 export default defineConfig({
   output: 'static',
   integrations: [react()],
+  vite: {
+    define: {
+      'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+      'process.env.SUPABASE_SERVICE_ROLE_KEY': JSON.stringify(process.env.SUPABASE_SERVICE_ROLE_KEY),
+      'process.env.PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(process.env.PUBLIC_SUPABASE_ANON_KEY),
+      'process.env.PUBLIC_SUPABASE_URL': JSON.stringify(process.env.PUBLIC_SUPABASE_URL)
+    }
+  }
 });
